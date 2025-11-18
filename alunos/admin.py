4419e93e74bd8ff584ffbe3bc6ebbbe9481a2f1c
rogeriodelphi <rogeriodelphi@gmail.com>
2025-11-18 09:21:46 -0300

    Criando app de Emprestimo

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
+from .models import Aluno
 
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'matricula', 'email', 'telefone')
+    search_fields = ['nome', 'matricula']
